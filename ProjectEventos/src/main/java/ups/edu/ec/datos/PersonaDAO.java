fd6af7157b3fbcb086f11688603ab3d5cda9da50
xavierllauca <xavierllauca@users.noreply.github.com>
2017-11-21 11:29:34 -0500

    dao

--- ProjectEventos/src/main/java/ups/edu/ec/datos/PersonaDAO.java
+++ ProjectEventos/src/main/java/ups/edu/ec/datos/PersonaDAO.java
@@ -4,13 +4,37 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import ups.edu.ec.modelo.Persona;
+
 @Stateless
 public class PersonaDAO {
 	
 	@Inject
 	private EntityManager em;
 	
+	public void insertar(Persona p) {
+		em.persist(p);
+	}
 	
+	public void actualizar(Persona p) {
+		
+		em.merge(p);
+	}
+	
+	public Persona leer(int id) {
+		
+		Persona p = em.find(Persona.class, id);
+		
+		return p;
+	}
+	
+	public void borrar (int id) {
+		
+		Persona p = leer(id);
+		
+		em.remove(p);
+		
+	}
 	
 	
 	
---
+++ ProjectEventos/src/main/java/ups/edu/ec/modelo/Persona.java
@@ -0,0 +1,12 @@
+package ups.edu.ec.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Persona {
+	
+	@Id
+	private int codigo;
+
+}
