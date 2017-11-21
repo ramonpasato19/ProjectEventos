967e87c0d1640fa3c5eebf2fee3892c4b1ad7b9e
xavierllauca <xavierllauca@users.noreply.github.com>
2017-11-21 11:04:31 -0500

    creacion DAO

--- ProjectEventos/src/main/java/ups/edu/ec/controlador/ArtistaControlador.java
+++ ProjectEventos/src/main/java/ups/edu/ec/controlador/ArtistaControlador.java
@@ -1,5 +1,6 @@
 package ups.edu.ec.controlador;
 
 public class ArtistaControlador {
+	
 
 }
---
+++ ProjectEventos/src/main/java/ups/edu/ec/datos/PersonaDAO.java
@@ -0,0 +1,18 @@
+package ups.edu.ec.datos;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@Stateless
+public class PersonaDAO {
+	
+	@Inject
+	private EntityManager em;
+	
+	
+	
+	
+	
+
+}
