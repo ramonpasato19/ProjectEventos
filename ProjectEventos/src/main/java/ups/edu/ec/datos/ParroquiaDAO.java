1e6ef6b3c3825dc71cd2ed7874b5646ee54ce31e
xavierllauca <xavierllauca@users.noreply.github.com>
2017-11-21 14:54:46 -0500

    dao parooquia

--- ProjectEventos/src/main/java/ups/edu/ec/datos/ParroquiaDAO.java
+++ ProjectEventos/src/main/java/ups/edu/ec/datos/ParroquiaDAO.java
@@ -4,12 +4,37 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
+import ups.edu.ec.modelo.Parroquia;
+
 @Stateless
 public class ParroquiaDAO {
 	
 	@Inject
 	private EntityManager em;
+	
+	public void insertar (Parroquia p) {
+		
+		em.persist(p);
+	}
+	
+	public void actualisar (Parroquia p) {
+		
+		em.merge(p);
+	}
+	
+	public Parroquia leer(int id) {
+		
+		Parroquia p = em.find(Parroquia.class, id);
+		
+		return p;
+	}
 
+	
+	public void eliminar(int id) {
+		Parroquia p = leer(id);
+	
+		em.remove(p);
+	}
 
 
 }
