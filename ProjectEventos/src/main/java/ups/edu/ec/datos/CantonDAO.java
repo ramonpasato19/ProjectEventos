55bebd5d62993de5fef0383814974a039c72d6f6
xavierllauca <xavierllauca@users.noreply.github.com>
2017-11-21 11:37:40 -0500

    dao canton

---
+++ ProjectEventos/src/main/java/ups/edu/ec/datos/CantonDAO.java
@@ -0,0 +1,42 @@
+package ups.edu.ec.datos;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import ups.edu.ec.modelo.Canton;
+
+@Stateless
+public class CantonDAO {
+	
+	@Inject
+	private EntityManager em;
+	
+	public void insertar(Canton c) {
+		
+		em.persist(c);
+	}
+	
+	public void actualizar(Canton c) {
+		
+		em.merge(c);
+		
+	}
+	
+	public Canton leer(int id) {
+		
+		Canton c = em.find(Canton.class, id);
+		
+		return c;
+		
+	}
+	
+	public void borarr(int id) {
+		
+		Canton c = leer(id);
+		
+		em.remove(c);
+		
+	}
+
+}
