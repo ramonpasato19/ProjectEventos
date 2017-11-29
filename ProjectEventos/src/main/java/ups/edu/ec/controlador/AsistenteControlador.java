8c009877aeb261501c1348268f17237f6c59ade7
mauriciomocha <33765461+mauriciomocha@users.noreply.github.com>
2017-11-28 20:06:11 -0500

    Update AsistenteControlador.java

--- ProjectEventos/src/main/java/ups/edu/ec/controlador/AsistenteControlador.java
+++ ProjectEventos/src/main/java/ups/edu/ec/controlador/AsistenteControlador.java
@@ -12,5 +12,29 @@ import ups.edu.ec.modelo.Asistente;
 @ManagedBean
 public class AsistenteControlador {
 	
-
+private AsistenteDAO asdao;
+	private Asistente asistente=null;
+	private List<Asistente> asistentes;
+	
+	public AsistenteDAO getAsdao() {
+		return asdao;
+	}
+	
+	public void setAsdao(AsistenteDAO asdao) {
+		this.asdao = asdao;
+	}
+	public Asistente getAsistente() {
+		return asistente;
+	}
+	public void setAsistente(Asistente asistente) {
+		this.asistente = asistente;
+	}
+	public List<Asistente> getAsistentes() {
+		return asistentes;
+	}
+	public void setAsistentes(List<Asistente> asistentes) {
+		this.asistentes = asistentes;
+	}
+	
+	
 }
