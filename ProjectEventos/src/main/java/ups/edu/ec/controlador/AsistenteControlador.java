017a71e8c91d75aa407796abe0567e038f62fd45
mauriciomocha <33765461+mauriciomocha@users.noreply.github.com>
2017-11-28 20:01:59 -0500

    Update AsistenteControlador.java

--- ProjectEventos/src/main/java/ups/edu/ec/controlador/AsistenteControlador.java
+++ ProjectEventos/src/main/java/ups/edu/ec/controlador/AsistenteControlador.java
@@ -1,6 +1,13 @@
 package ups.edu.ec.controlador;
 
+
+
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+
+import ups.edu.ec.datos.AsistenteDAO;
+import ups.edu.ec.modelo.Asistente;
 
 @ManagedBean
 public class AsistenteControlador {
