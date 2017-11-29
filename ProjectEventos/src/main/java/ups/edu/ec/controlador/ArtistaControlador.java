db7aa87ff50eacd588186216ecbb5cc8e6718340
mauricio <mauricio@172.16.220.33>
2017-11-28 19:08:45 -0500

    controlador

--- ProjectEventos/src/main/java/ups/edu/ec/controlador/ArtistaControlador.java
+++ ProjectEventos/src/main/java/ups/edu/ec/controlador/ArtistaControlador.java
@@ -1,8 +1,56 @@
 package ups.edu.ec.controlador;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+
+import org.jboss.arquillian.core.api.annotation.Inject;
+
+import ups.edu.ec.datos.ArtistaDAO;
+import ups.edu.ec.modelo.Artista;
+
+
 
 @ManagedBean
 public class ArtistaControlador {
 	
+	@Inject
+	private ArtistaDAO adao;
+	private Artista artista=null;
+	private List<Artista>artistas;
+	
+	
+	
+	
+	
+	
+	
+	public ArtistaDAO getAdao() {
+		return adao;
+	}
+	public void setAdao(ArtistaDAO adao) {
+		this.adao = adao;
+	}
+	public Artista getArtista() {
+		return artista;
+	}
+	public void setArtista(Artista artista) {
+		this.artista = artista;
+	}
+	public List<Artista> getArtistas() {
+		return artistas;
+	}
+	public void setArtistas(List<Artista> artistas) {
+		this.artistas = artistas;
+	}
+	
+	
+	
+	
+	public String Guardar() {
+		System.out.println(artista);
+		
+		return null;
+		
+	}
 
 }
