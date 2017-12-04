2aebdaa73b719d28e9945ef1645e4cee24569e46
mauricio <mauricio@192.168.1.102>
2017-12-03 21:18:05 -0500

    concursogetter

--- ProjectEventos/src/main/java/ups/edu/ec/modelo/Concurso.java
+++ ProjectEventos/src/main/java/ups/edu/ec/modelo/Concurso.java
@@ -24,6 +24,50 @@ public class Concurso {
 	private String descripcion;
 
 
+	
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+
+
 	@Override
 	public String toString() {
 		return "Concurso [codigo=" + codigo + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
