998748b9c086e30aa8b790e9afd8346da8e7e1d5
mauricio <mauricio@192.168.1.102>
2017-12-03 21:25:05 -0500

    concursodao

---
+++ ProjectEventos/src/main/java/ups/edu/ec/datos/ConcursoDAO.java
@@ -0,0 +1,18 @@
+package ups.edu.ec.datos;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import ups.edu.ec.modelo.Concurso;
+import ups.edu.ec.modelo.Evento;
+
+
+@Stateless
+public class ConcursoDAO {
+
+	
+}
