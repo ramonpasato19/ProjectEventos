e4dc08126f51a107c855fc21809e610512c4b742
Xavier Llauca <xllauca@est.ups.edu.ec>
2018-01-03 12:45:32 -0500

    Add files via upload

---
+++ ProjectEventos/src/main/java/ups/edu/ec/controlador/UsuarioControlador.java
@@ -0,0 +1,68 @@
+package ups.edu.ec.controlador;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import ups.edu.ec.datos.UsuarioDAO;
+import ups.edu.ec.modelo.Usuario;
+
+@ManagedBean
+@ViewScoped
+public class UsuarioControlador implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7158501497498000006L;
+	
+	@EJB
+	private UsuarioDAO usuariodao;
+	
+	private Usuario usuario;
+	
+	public List<Usuario> getUsuarios(){
+		return usuariodao.listarTodos();
+	}
+
+	
+	@PostConstruct
+	public void init() {
+		usuario = new Usuario();
+		
+	}
+
+	public void salvar() {
+		try {
+			usuariodao.salvar(usuario);
+			FacesContext.getCurrentInstance().addMessage("", new FacesMessage("Exitoso", "Usuario Guardado con Exito"));
+
+		} catch (Exception e) {
+			FacesContext.getCurrentInstance().addMessage("", new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Usuario no de a guardado"));
+		}
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
